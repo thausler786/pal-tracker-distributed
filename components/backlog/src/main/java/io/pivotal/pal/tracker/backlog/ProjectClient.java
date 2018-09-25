310d80a64326d813625d140fa3be6a8d7d326c4d
Tim Hausler <thausler@pivotal.io>
2018-09-25 16:36:55 -0700

    Add circuit breaker
    Signed-off-by: Steven Locke <slocke@pivotal.io>

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,9 +1,13 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import org.springframework.web.client.RestOperations;
 
 public class ProjectClient {
 
+    private static final ConcurrentMap<Long, ProjectInfo> cache = new ConcurrentHashMap<Long, ProjectInfo>();
     private final RestOperations restOperations;
     private final String endpoint;
 
@@ -12,7 +16,14 @@ public class ProjectClient {
         this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo result = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.put(projectId, result);
+        return result;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cache.get(projectId);
     }
 }
